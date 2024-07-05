fe56bdb8b687d88982571d41cd142d80ed82f412
Peng Zijun <2200012909@stu.pku.edu.cn>
2024-07-05 20:55:16 +0800

    Add GPL license and copyright infomation

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,17 @@
+/* Str2table core crate
+ * Copyright (C) 2024 Peng Zijun, Xia Tingxuan
+ *
+ * This file is part of Str2table.
+ * Foobar is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * Str2table is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Foobar.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 pub mod export;
 pub mod setting;
 pub mod table;
--- core/src/setting.rs
+++ core/src/setting.rs
@@ -1,3 +1,17 @@
+/* Str2table core crate for settings
+ * Copyright (C) 2024 Peng Zijun, Xia Tingxuan
+ *
+ * This file is part of Str2table.
+ * Foobar is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * Str2table is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Foobar.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 //! # Setting
 //! This module is used to set the setting of the table. Settings can be set by
 //! commandline arguments temperarily or by a pre-set configuration file. You can
